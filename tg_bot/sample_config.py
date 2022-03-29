3523fbf658eb99830248d976553760fc707babfe
sachin-0027 <95533165+sachin-0027@users.noreply.github.com>
2022-03-29 07:15:24 -0500

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "Y5191392440:AAHwI_sPwXsT4ATgOSdaA5AmBYFbZyGqHzo"
+    OWNER_ID = "669328965"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "bgmworldbot"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -23,8 +23,8 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    SUDO_USERS = [669328965]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [669328965]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
